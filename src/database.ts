3165770010f3dd0155db565ddf8288901f4ff6d2
Lucas Bonfanti <lucasbonfanti97@gmail.com>
2023-03-01 21:30:44 -0300

    aula typescript-I

---
+++ src/database.ts
@@ -0,0 +1,44 @@
+import { TProduct, TPurchase, TUser } from "./types";
+
+export const user: Array<TUser> = [
+    {
+        id: "clienteA",
+        email: "lucas@email.com",
+        password: "password123"
+    },
+    {
+        id: "clienteB",
+        email: "joao@email.com",
+        password: "password321"
+    }
+]
+
+export const product: Array<TProduct> = [
+    {
+        id: "produtoA",
+        name: "Relogio",
+        price: 30,
+        category: "utilidades"
+    },
+    {
+        id: "produtoB",
+        name: "Panela",
+        price: 70,
+        category: "cozinha"
+    }
+]
+
+export const purchase: Array<TPurchase> = [
+    {
+        userId: "clienteA",
+        productId: "produtoA",
+        quantity: 2,
+        totalPrice: 60
+    },
+    {
+        userId: "clienteB",
+        productId: "produtoB",
+        quantity: 3,
+        totalPrice: 210
+    }
+]
